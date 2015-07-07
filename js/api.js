d4e37f4f9830f5ec8f116bbb83856fd83c737c0a
Anthony Wong <anthonywong555@gmail.com>
2015-07-07 12:53:36 -0400

    Add api.js

---
+++ js/api.js
@@ -0,0 +1,13 @@
+/**
+ * api.js
+ * @author Anthony Wong
+ * @description This is for debugging purpose
+ */
+
+/**
+ * Runs the given code in JavaEngie
+ * @param  {String} code [A JavaScript Code]
+ */
+function runCode(code){
+
+}
