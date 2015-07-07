9fd6d5308777f2e4731e6e3f6f301b86844c8c57
Anthony Wong <anthonywong555@gmail.com>
2015-07-07 00:49:18 -0400

    Sending SQL via AJAX works!

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
-var express = require('express');
-    app = express();
+var express 	= require('express');
+var app 		= express();
+var mysql 		= require('mysql');
+var bodyParser = require('body-parser')
+var connection  = mysql.createConnection({
+	host: 		'localhost',
+	user: 		'admin',
+	password: 	'whatsup',
+	database: 	'students',
+});
+
+app.use(bodyParser());
+
+/* MySQL conection */
+
+//connection.connect();
+/*
+connection.query('CREATE TABLE pet (name VARCHAR(20))', function(err, rows, fields) {
+	if(!err) {
+		console.log('Rows: ', rows);
+		console.log('Fields: ', fields);
+	} else {
+		console.log('Error while performing Query.', err);
+	}
+});*/
+
+//connection.end();
 
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/index4.html');
@@ -9,7 +34,24 @@ app.use('/ace-builds', express.static(__dirname + '/ace-builds'));
 app.use('/glyphicons', express.static(__dirname + '/glyphicons'));
 
 
-//app.post('/api/meetups', meetupsController.create);
+app.post('/send/data', function (req, res){
+	connection.connect();
+	connection.query(req.body.code1, function(err, rows, fields) {
+		connection.end();
+  		if (!err){
+  			console.log('Works');
+  			console.log("Rows: ", rows);
+  			console.log("Fields:", fields);
+  			res.json({"rows" : rows, "fields" : fields, "name" : "Anthony"});
+			return;
+  		} else {
+  			console.log('Nope');
+  			console.log("err:", err);
+  			res.json(err);
+			return;
+  		}
+  	});
+});
 
 app.listen(4000, function() {
     console.log('I\'m Listening...');
