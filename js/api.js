e916259103cf8c9cf1535434e66d9951773a0c3f
Anthony Wong <anthonywong555@gmail.com>
2015-07-09 00:12:10 -0400

    Hopefully this works

---
+++ js/api.js
@@ -0,0 +1,124 @@
+/**
+ * API.JS
+ * @author [Anthony Wong]
+ */
+
+/**
+ * [When editor detectes a keystoke]
+ */
+function editorKeyPressed(){
+  document.getElementById("save_icon").src = "lib/glyphicons/png/glyphicons-447-floppy-save.png";
+  document.getElementById("save_button").className ="btn btn-primary";
+}
+
+/**
+ * [Ctrl-S]
+ * @return {[type]} [description]
+ */
+function saveButtonPress(){
+  var evtobj = window.event? event : e
+  if (evtobj.keyCode == 83 && evtobj.ctrlKey) {
+    saveButtonOnClicked();
+  } 
+}
+
+/**
+ * [Run Button ActionListner]
+ */
+function runButtonOnClicked(){
+  runCode(editor.getValue());
+}
+
+/**
+ * [Runs the JavaScript Engine based on the code]
+ * @param  {String} code [A JavaScript expression, variable, statement, or sequence of statements]
+ */
+function runCode(code){
+	eval(code);
+}
+
+/**
+ * [Save Button ActionListner]
+ */
+function saveButtonOnClicked(){
+  console.log("saveButtonOnClicked");
+  document.getElementById("save_icon").src = "lib/glyphicons/png/glyphicons-445-floppy-saved.png";
+  document.getElementById("save_button").className ="btn btn-success";
+  console.log(JSON.stringify({ "code": editor.getValue() }));
+  /* Modify the code */
+  var code = editor.getSession().getValue();
+  console.log("code: " + code);
+  var request = "INSERT INTO code (code) VALUES (" + code + ")";
+
+  /* Send the request */
+  $.ajax({
+    type: "POST",
+      url: "/save/code",          
+      data: JSON.stringify({"author" : "Anthony",  "code": code }),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function(data){
+        console.log(data);
+      },
+      failure: function(errMsg) {
+        console.log(errMsg);
+      }
+  });
+}
+
+/**
+ * [Sends a given query to the server via AJAX]
+ * @param  {String} query [Query Instruction]
+ * @return {response}       [Returns the server response]
+ */
+function sendQuery(query){
+  console.log("sendQuery");
+  $.ajax({
+    type: "POST",
+      url: "/query",          
+      data: JSON.stringify({ "query": query }),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function(data){
+        console.log(data);
+        if(data.hasOwnProperty('rows')){
+          console.log("table updated");
+          $('#table').bootstrapTable('load', data.rows);
+
+          /*
+          for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < columns; j++) {
+              var square = i + "," + j;
+              document.getElementById(square).src ="lib/glyphicons/png/glyphicons-262-buoy.png";
+            }
+          }*/
+
+          for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < columns; j++) {
+              console.log("rows: " + i + "columns: " + j);
+              grid[i][j] = 0;
+            }
+          }
+          
+          if(data.rows.length > 0){
+            // Load the boat image 
+            for(var i = 0; i < data.rows.length; i++){
+              var x = data.rows[i].x;
+              var y = data.rows[i].y;
+              grid[y][x] = 1;
+              console.log("x: " + x + "y: " + y);
+              var id =  x + ',' + y ;
+              
+              document.getElementById(id).src ="lib/glyphicons/png/glyphicons-256-boat.png";
+            }
+          }
+        }
+
+        return data;
+      },
+      failure: function(errMsg) {
+        console.log("error");
+          return errMsg;
+      }
+  });
+}
---
+++ js/grid.js
@@ -0,0 +1,56 @@
+/**
+ * Grid.js
+ * @author [Anthony Wong]
+ */
+/* First Loop with innerHTML */
+var rows = 3;
+var columns = 5
+var content = '';
+var square = '';
+var group = '<div class="btn-group btn-group-lg" role="group" aria-label="..." id="buttons">';
+
+for (var i = 0; i < rows; i++) {
+  content += group;
+  for (var j = 0; j < columns; j++) {
+    square = i + "," + j;
+    //content += '<button type="button" class="btn btn-primary" id="' + square + '"' + 'onclick="clicked(' + square + ')">'+ i + ',' + j + '</button>';
+    content += '<button type="button" class="btn btn-info"' + 'id="' + square + '"' + 'onclick="clicked(' + square + ')">' + '<img ' + 'src="lib/glyphicons/png/glyphicons-262-buoy.png">' + '</button>';
+    //content += '<button type="button" class="btn btn-primary"' + 'onclick="clicked(' + square + ')">' + '</button>';
+  }
+  content += '</div></br>';
+}
+
+document.getElementById('your_grid').innerHTML = content;
+//document.getElementById('your_grid').innerHTML = content;
+
+function buildGrid(numrows, numcols, initial){
+    var arr = [];
+    for (var i = 0; i < numrows; ++i){
+        var columns = [];
+        for (var j = 0; j < numcols; ++j){
+          columns[j] = 0;
+        }
+        arr[i] = columns;
+    }
+    return arr;
+}
+
+var grid = buildGrid(rows, columns, 0);
+
+/**
+ * Call function when the user clicks on the grid
+ *
+ **/
+function clicked(row, column) {
+  var square = row + ',' + column;
+  console.log(grid[row][column]);
+
+  if(grid[row][column] == 1){
+    // isAShip
+    document.getElementById(square).className = "btn btn-success";
+    document.getElementById(square).innerHTML = "HIT!";
+  } else {
+    document.getElementById(square).className = "btn btn-danger";
+    document.getElementById(square).innerHTML = '<span class="glyphicon glyphicon-remove"  aria-hidden="true"></span>';
+  }
+}
